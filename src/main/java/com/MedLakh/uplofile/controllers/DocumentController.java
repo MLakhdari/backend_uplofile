7898e956a6ec929b682701351ba71d887f57e296
MLakhdari <mlakhdari1998@gmail.com>
2023-03-20 01:59:12 +0000

    v3

--- src/main/java/com/MedLakh/uplofile/controllers/DocumentController.java
+++ src/main/java/com/MedLakh/uplofile/controllers/DocumentController.java
@@ -37,30 +37,29 @@ import com.amazonaws.services.s3.transfer.Upload;
 
 @RestController
 @RequestMapping("/api/services/documents")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class DocumentController {
 
-	@Autowired
+    @Autowired
     private AmazonS3 amazonS3;
 
     @Value("${digitalocean.spaces.bucket-name}")
     private String bucketName;
-    
+
     @Value("${frontend.url}")
     private String frontendUrl;
-    
-    
+
     private Map<String, Float> progressUpload = new HashMap<String, Float>();
     private Map<String, DownloadProgress> progressDownload = new HashMap<String, DownloadProgress>();
-    
+
     @GetMapping("/upload-progress/{id}")
     public ResponseEntity<Object> getUploadProgress(@PathVariable(required = true) String id) {
-    	return ResponseEntity.ok(progressUpload.get(id));
+        return ResponseEntity.ok(progressUpload.get(id));
     }
-    
+
     @GetMapping("/download-progress/{id}")
     public ResponseEntity<Object> getDownloadProgress(@PathVariable(required = true) String id) {
-    	return ResponseEntity.ok(progressDownload.get(id));
+        return ResponseEntity.ok(progressDownload.get(id));
     }
 
     @PostMapping
@@ -70,109 +69,119 @@ public class DocumentController {
             metadata.setContentLength(file.getSize());
             metadata.setContentType(file.getContentType());
             String fileName = file.getOriginalFilename() + "_" + UUID.randomUUID().toString();
-            
+
             TransferManager transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3).build();
             Upload upload = transferManager.upload(bucketName, fileName, file.getInputStream(), metadata);
 
             this.progressUpload.put(id, 0f);
             while (!upload.isDone()) {
-            	this.progressUpload.put(id, (float) upload.getProgress().getPercentTransferred());
+                this.progressUpload.put(id, (float) upload.getProgress().getPercentTransferred());
                 try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					System.out.println("Erreur thread sleep");
-				}
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Erreur thread sleep");
+                }
             }
             try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				System.out.println("Erreur thread sleep");
-			}
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Erreur thread sleep");
+            }
             this.progressUpload.remove(id);
-            
+
             String fileUrl = frontendUrl + "?file=" + UriUtils.encode(fileName, StandardCharsets.UTF_8);
             return ResponseEntity.ok(fileUrl);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
         }
     }
-    
+
     @GetMapping
-    public ResponseEntity<Object> downloadFile(@RequestParam(required = true) String file, @RequestParam("id") String id) {
-    	File tempFile = null;
-    	byte[] fileBytes = null;
-    	try {
-    		String fileName = UriUtils.decode(file, StandardCharsets.UTF_8);
+    public ResponseEntity<Object> downloadFile(@RequestParam(required = true) String file,
+            @RequestParam("id") String id) {
+        File tempFile = null;
+        byte[] fileBytes = null;
+        try {
+            String fileName = UriUtils.decode(file, StandardCharsets.UTF_8);
             S3Object s3Object = amazonS3.getObject(bucketName, fileName);
-            
+
             TransferManager transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3).build();
             tempFile = File.createTempFile("download", ".tmp");
-            
+
             Download download = transferManager.download(bucketName, fileName, tempFile);
-            
+
             this.progressDownload.put(id, new DownloadProgress(0f, download.getProgress().getBytesTransferred(),
-            		download.getProgress().getTotalBytesToTransfer()));
+                    download.getProgress().getTotalBytesToTransfer()));
             while (!download.isDone()) {
-            	this.progressDownload.put(id, new DownloadProgress((float) download.getProgress().getPercentTransferred(),
-            			download.getProgress().getBytesTransferred(), download.getProgress().getTotalBytesToTransfer()));
+                this.progressDownload.put(id,
+                        new DownloadProgress((float) download.getProgress().getPercentTransferred(),
+                                download.getProgress().getBytesTransferred(),
+                                download.getProgress().getTotalBytesToTransfer()));
                 try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					System.out.println("Erreur thread sleep");
-				}
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Erreur thread sleep");
+                }
             }
             try {
-				Thread.sleep(1500);
-			} catch (InterruptedException e) {
-				System.out.println("Erreur thread sleep");
-			}
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                System.out.println("Erreur thread sleep");
+            }
             this.progressDownload.remove(id);
-            
+
             fileBytes = Files.readAllBytes(tempFile.toPath());
-            
+
             HttpHeaders headers = new HttpHeaders();
             String _fileName = fileName.substring(0, fileName.lastIndexOf("_"));
             headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + _fileName + "\"");
-            
+
             return ResponseEntity.ok()
                     .contentLength(tempFile.length())
                     .contentType(MediaType.parseMediaType(s3Object.getObjectMetadata().getContentType()))
                     .headers(headers)
                     .body(fileBytes);
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse("A technical error has occurred. Please contact support."));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new MessageResponse("A technical error has occurred. Please contact support."));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("The requested document was not found."));
-        }
-        finally {
-        	if(tempFile != null) {
-        		tempFile.delete();
-        	}
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new MessageResponse("The requested document was not found."));
+        } finally {
+            if (tempFile != null) {
+                tempFile.delete();
+            }
         }
     }
 
-
-    
-    /*@GetMapping("/{fileName}")
-    public ResponseEntity<Object> downloadFiles(@PathVariable(required = true) String fileName) {
-        try {
-            S3Object s3Object = amazonS3.getObject(bucketName, fileName);
-            S3ObjectInputStream stream = s3Object.getObjectContent();
-            ByteArrayResource resource = new ByteArrayResource(IOUtils.toByteArray(stream));
-            HttpHeaders headers = new HttpHeaders();
-            String _fileName = fileName.substring(0, fileName.lastIndexOf("_"));
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + _fileName + "\"");
-
-            return ResponseEntity.ok()
-                    .contentLength(s3Object.getObjectMetadata().getContentLength())
-                    .contentType(MediaType.parseMediaType(s3Object.getObjectMetadata().getContentType()))
-                    .headers(headers)
-                    .body(resource);
-        } catch (IOException e) {
-        	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse(
-        			"A technical error has occurred. Please contact support."));
-        } catch (Exception e) {
-        	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("The requested document was not found."));
-        }
-    }*/
+    /*
+     * @GetMapping("/{fileName}")
+     * public ResponseEntity<Object> downloadFiles(@PathVariable(required = true)
+     * String fileName) {
+     * try {
+     * S3Object s3Object = amazonS3.getObject(bucketName, fileName);
+     * S3ObjectInputStream stream = s3Object.getObjectContent();
+     * ByteArrayResource resource = new
+     * ByteArrayResource(IOUtils.toByteArray(stream));
+     * HttpHeaders headers = new HttpHeaders();
+     * String _fileName = fileName.substring(0, fileName.lastIndexOf("_"));
+     * headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +
+     * _fileName + "\"");
+     * 
+     * return ResponseEntity.ok()
+     * .contentLength(s3Object.getObjectMetadata().getContentLength())
+     * .contentType(MediaType.parseMediaType(s3Object.getObjectMetadata().
+     * getContentType()))
+     * .headers(headers)
+     * .body(resource);
+     * } catch (IOException e) {
+     * return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new
+     * MessageResponse(
+     * "A technical error has occurred. Please contact support."));
+     * } catch (Exception e) {
+     * return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new
+     * MessageResponse("The requested document was not found."));
+     * }
+     * }
+     */
 }
